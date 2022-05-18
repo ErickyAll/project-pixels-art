64e1ddf0cf743cd8390b14ea6eee7eb2abd3e38a
Erick Almeida <erick227.ea21@gmail.com>
2022-05-18 01:55:49 -0300

    Conclusão 1.0
    Co-authored-by: Leandro Liduvino <leandrolid@users.noreply.github.com>

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function createPallete() {
     const element = document.createElement('div');
     element.style.backgroundColor = item;
     element.classList.add('color');
-
+    if (item == 'black') {
+      element.classList.add('selected');
+    }
     return element;
   });
 
@@ -42,14 +44,64 @@ creatPixelBoard(5);
 
 function getBoardLength() {
   const getForm = document.querySelector('#send');
+
   getForm.addEventListener('submit', (ev) => {
     ev.preventDefault();
-    const getInput = document.getElementById('lengthBoard');
+
+    const getInput = document.getElementById('board-size');
+
+    if (!getInput.value) {
+      alert('Board inválido!');
+
+      return;
+    }
+
     const clearPB = document.querySelectorAll('.line');
+
     clearPB.forEach((item) => item.remove());
     const inputNumber = Number(getInput.value);
+
     creatPixelBoard(inputNumber);
+    drawPixels();
   });
 }
 
 getBoardLength();
+
+function selectColor() {
+  const elements = document.querySelectorAll('.color');
+  elements.forEach((element) => {
+    element.addEventListener('click', (ev) => {
+      const prevElement = document.querySelector('.selected');
+      prevElement.classList.remove('selected');
+      ev.target.classList.add('selected');
+    });
+  });
+}
+
+selectColor();
+
+function drawPixels() {
+  const pixelSelected = document.querySelectorAll('.pixel');
+  pixelSelected.forEach((pixel) => {
+    pixel.addEventListener('click', (ev) => {
+      const colorSelected = document.querySelector('.selected');
+      const colorOfColor = colorSelected.style.backgroundColor;
+      ev.target.style.backgroundColor = colorOfColor;
+    });
+  });
+}
+
+drawPixels();
+
+function clearBoard() {
+  const button = document.querySelector('#clear-board');
+  button.addEventListener('click', (ev) => {
+    const clearPB = document.querySelectorAll('.pixel');
+    clearPB.forEach((item) => {
+      item.style.backgroundColor = 'white';
+    });
+  });
+}
+
+clearBoard();
